2d5377d392d558816b20ea4478ed41b5d5f27457
sujwa <sujwa@MSI>
2022-11-15 16:23:09 +0530

    updated

--- src/zc/training/practice/Palindrome.java
+++ src/zc/training/practice/Palindrome.java
@@ -4,23 +4,23 @@ public class Palindrome {
 
 	 
 
-	public static void isPalindrome(int sum, int num, int num1) {
-		if (num1 == 0) {
+	public static void isPalindrome(int sum, int num, int temp) {
+		if (temp == 0) {
 			if (num == sum) {
 				System.out.println("It is palindrome : " + sum);
 			}
 			return;
 		}
-		int r = num1 % 10;
+		int r = temp % 10;
 		sum = sum * 10 + r;
-		num1 = num1 / 10;
-		isPalindrome(sum, num, num1);
+		temp = temp / 10;
+		isPalindrome(sum, num, temp);
 	}
 
 	public static void main(String[] args) {
 		int sum = 0;
 		 int num = 131;
-		 int num1 = num;
-		isPalindrome(sum,num,num1);
+		 int temp = num;
+		isPalindrome(sum,num,temp);
 	}
 }
