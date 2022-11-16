9a0c00a3da14aef54b735c7f78d7b49598a011e2
sujwa <sujwa@MSI>
2022-11-16 08:55:32 +0530

    edited

--- src/zc/training/practice/PrimeWithOutDebug.java
+++ src/zc/training/practice/PrimeWithOutDebug.java
@@ -7,7 +7,7 @@ public class PrimeWithOutDebug {
 		for (i = 1; i <= number; i++) {
 			fact = fact * i;
 		}
-		System.out.println("Factorial of " + number + " is: " + fact);
+		System.out.println("Factorial of the given " + number + " is: " + fact);
 	}
 
 	public static void isPrime() {
